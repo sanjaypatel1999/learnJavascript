75f194fe248aa1ae740a46e57d8dff8ad56b9bd5
Sanjay Patel <110016937+sanjaypatel1999@users.noreply.github.com>
2025-02-02 12:24:21 +0530

    operation of javascripts

---
+++ Operation/conversionoperations.js
@@ -0,0 +1,102 @@
+//using var
+// var score=40
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// var valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)
+
+// var score="40"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// var valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)  // 40
+
+// var score="40ab"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// var valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)   //NaN
+
+// var score=true
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// var valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)   //1
+
+//using let
+
+// let score=40
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// var valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)
+
+// let score="40"
+// console.log(typeof score)
+
+
+// let valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)
+
+// let score="40abc"
+// console.log(typeof score)
+
+
+// let valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)   //NaN
+
+// let score=null
+// console.log(typeof score)
+
+
+// let valueNum=Number(score)
+// console.log(typeof valueNum)
+// console.log(valueNum)   // 0
+
+
+// var isboolean=1
+// var isboolean1=Number(isboolean)
+// console.log(isboolean1);  //1
+
+
+// var isboolean=1
+// var isboolean1=Boolean(isboolean)
+// console.log(isboolean1);  //true
+
+
+// var isboolean="sanjay"
+// var isboolean1=Boolean(isboolean)
+// console.log(isboolean1);  //true
+
+
+// var isboolean=""
+// var isboolean1=Boolean(isboolean)
+// console.log(isboolean1);  //false
+
+// var istirng=33
+// var isstringnew=String(istirng)
+// console.log(isstringnew)  //33
+// console.log(typeof isstringnew)
+
+
+var istirng=true
+var isstringnew=String(istirng)
+console.log(typeof isstringnew)
+console.log(isstringnew)  //true
