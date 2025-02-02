0c4aaeaf3da687f7b1a0cbd3f21bbd2947655294
Sanjay Patel <110016937+sanjaypatel1999@users.noreply.github.com>
2025-02-02 16:58:50 +0530

    update operation

---
+++ Operation/Comparision.js
@@ -0,0 +1,32 @@
+// console.log("2" >=1);   //true
+
+
+// console.log("2" >="3");   //false
+
+// console.log(2 >="1");   //true
+
+
+// console.log(null >="1");   //false
+// console.log(null >=0);   //true
+
+// console.log(null <=0);   //true
+
+// console.log(null <="0");   //true
+
+// console.log(null ==0);   //false
+
+// console.log(null >0);   //false
+// console.log(null < 0);   //false
+
+// console.log(undefined < 0);   //faLSE
+// console.log(undefined > 0);   //false
+// console.log(undefined <= 0);   //false
+
+// console.log(undefined >= 0);   //false
+
+// console.log(undefined == undefined); //TRUE
+
+// console.log(undefined <= undefined); //false
+// console.log(NaN ==NaN);  //false
+
+console.log(NaN >=0);   //false
---
+++ Operation/profix&postfix.js
@@ -0,0 +1,11 @@
+// console.log(true);  //true
+
+// console.log(+true);  // 1
+
+let i=1;
+console.log(i++);
+// console.log(typeof i);
+
+// console.log(++i);
+console.log(typeof i);
+
