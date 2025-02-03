51f0a8d989a6e413f34dd6bbb5a6b5d5282ff52e
Sanjay Patel <110016937+sanjaypatel1999@users.noreply.github.com>
2025-02-03 22:45:03 +0530

    update array

--- Operation/Array/array.js
+++ Operation/Array/array.js
@@ -12,7 +12,7 @@
 // myarray.pop()
 // console.log(myarray);
 
-const myarray=[0,1,9,3,4,5,6,true,"sanjay"]
+// const myarray=[0,1,9,3,4,5,6,true,"sanjay"]
 
 // console.log(myarray.indexOf("sanjay")); //output 8
 
@@ -20,7 +20,17 @@ const myarray=[0,1,9,3,4,5,6,true,"sanjay"]
 
 
 
-console.log(myarray.includes(true)); //output true
+// console.log(myarray.includes(true)); //output true
 
 
 
+const myarray1=[1,2,5,6,7,8,2]
+console.log(myarray1);
+
+// const myarray2=myarray1.slice(1,5)
+// console.log("A " + myarray1);  //A 1,2,5,6,7,8,2   oringinal array not changed
+// console.log("Output "+myarray2);   //Output 2,5,6,7
+
+const myarray2=myarray1.splice(1,5)
+console.log("A " + myarray1);  //A 1,2  oringinal array was changed
+console.log("Output "+myarray2);  //Output 2,5,6,7,8
