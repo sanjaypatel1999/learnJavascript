88edd501736bc168c9b9e2293e4f73f7f0777d46
Sanjay Patel <110016937+sanjaypatel1999@users.noreply.github.com>
2025-02-05 22:33:49 +0530

    scope js

--- Operation/object/firstobj.js
+++ Operation/object/firstobj.js
@@ -5,3 +5,5 @@ const obj={
 Object.freeze(obj)   //freez first value and not change other time.
 obj.prop=44;
 console.log(obj.prop);
+
+
---
+++ Operation/object/scope.js
@@ -0,0 +1,12 @@
+function one()
+{
+    const username="hitsh"
+
+    function two()
+    {
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
