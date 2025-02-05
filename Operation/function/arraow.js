ce3eb72aaee6386e7b8ef100314b8f8ad8345f5d
Sanjay Patel <110016937+sanjaypatel1999@users.noreply.github.com>
2025-02-06 00:00:36 +0530

    arrow js

---
+++ Operation/function/arraow.js
@@ -0,0 +1,57 @@
+// const user={
+//     usrname:"hitesh",
+//     price:999,
+//     welcomemessage:function()
+//     {
+//         console.log(`${this.usrname} welcome to website`);
+//         console.log(this);
+        
+//     }
+// }
+// user.welcomemessage()
+
+// user.usrname="sam"
+// user.welcomemessage()
+
+
+// function one()
+// {
+//     console.log(this);
+    
+// }
+// one()
+
+
+// const one=function()
+// {
+//     let username="histesh"
+//     console.log(this.username);
+// }
+// one()    //undefined
+
+// const one=()=>
+// {
+//     let username="histesh"
+//     console.log(this.username);
+// }
+// one()    //undefined
+
+
+
+// const one=()=>
+//     {
+//         let username="histesh"
+//         console.log(this);
+//     }
+//     one()    //   {}
+
+
+// const Addsum=(num1,num2) =>
+// {
+//     return num1+num2
+// }
+// console.log(Addsum(3,5));
+
+
+const Addsum=(num1,num2) =>  num1+num2
+console.log(Addsum(3,5));
--- Operation/object/scope.js
+++ Operation/object/scope.js
@@ -1,12 +1,26 @@
-function one()
-{
-    const username="hitsh"
+// function one()
+// {
+//     const username="hitsh"
 
-    function two()
-    {
-        const website="youtube"
-        console.log(username);
-    }
-    // console.log(website);
-    two()
+//     function two()
+//     {
+//         const website="youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+
+function addone(num)
+{
+    return num+1
 }
+addone(5)
+
+const addtwo=num=function(num)
+{
+    return num+2
+}
+addtwo(5)
